0984547f1ad8af0a937fa9166cdec855ca625b81
Ranbir singh <ranbirsingh9156@gmail.com>
2025-06-08 01:36:33 +0530

    Initial commit: Appointment Scheduler with calendar and animations

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/appointment-scheduler', {
+    const conn = await mongoose.connect('mongodb+srv://appointment:appointment123@cluster0.mongodb.net/appointment-scheduler?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
